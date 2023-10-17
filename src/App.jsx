bc81dec3a7ef115180e3ce35489732e354a5018f
Nazmul Alam <shawonkst15@gmail.com>
2023-10-17 08:46:46 +0600

    add fie

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Home } from './ui/Home';
 import Menu from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import CreateOrder from './features/order/CreateOrder';
+import Order from './features/order/Order';
 
 const router = createBrowserRouter([
 	{
@@ -11,6 +14,9 @@ const router = createBrowserRouter([
 		path: '/menu',
 		element: <Menu />,
 	},
+	{ path: '/cart', element: <Cart /> },
+	{ path: '/order/new', element: <CreateOrder /> },
+	{ path: '/order', element: <Order /> },
 ]);
 function App() {
 	return <RouterProvider router={router} />;
